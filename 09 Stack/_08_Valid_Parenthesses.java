65434d0dc5c33d91eddf786b0c6403a56e98985a
GopalNavetia <work.gopalnavetia@gmail.com>
2026-06-15 14:32:39 +0530

    Add Stack Solutions

---
+++ 09 Stack/_08_Valid_Parenthesses.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class _08_Valid_Parenthesses{
+
+  public static boolean isValid(String str) {
+        Stack<Character> s = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            // Push when Open Bracket
+            if (ch == '(' || ch == '{' || ch == '[') {
+                s.push(ch);
+            } else {
+                if (s.isEmpty()) {
+                    return false;
+                }
+
+                // Pop when Close Bracket and making pair
+                if (ch == ')' && s.peek() == '(' ||
+                        ch == '}' && s.peek() == '{' ||
+                        ch == ']' && s.peek() == '[') {
+                    s.pop();
+                } else {
+                    return false;
+                }
+            }
+
+        }
+        if (s.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = new String("({}{}())");
+
+        System.out.println(isValid(str));
+    }
+}
